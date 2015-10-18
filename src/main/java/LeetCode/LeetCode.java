00b5200b638fbeac2e85285b5df82b29cad30463
surface <zhumeng1989@gmail.com>
2015-10-18 22:37:31 +0800

    ADD: LeetCode

---
+++ src/main/java/LeetCode/LeetCode.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+/**
+ * Created by Meng on 2015/9/4.
+ * LeetCode solutions (https://leetcode.com)
+ */
+public class LeetCode {
+    /**
+     * 纯代码逻辑的题目: 1, 2, 4, 5, 6, 7, 8, 9, 12, 13, 20, 21, 26, 27, 28, 36
+     *
+     * 有思考意义的题目:
+     *         简单                一般                困难
+     *     3(Two Array Pointer)
+     *     17(Recursion)
+     *     22(Recursion)
+     *     19(Tow List Pointer)
+     *     23(MinHeap)
+     *     24(Linked List)
+     *                        30(Two Array Pointer)[Longest Substring Which Contains K Unique Characters]
+     *                        32(Stack/Two Array Pointer + Bidirectional)
+     *                        33(Binary Search)
+     *     34(Binary Search)
+     *     35(Binary Search)
+     *     38(Recursion)
+     *                        39(DFS)
+     *                        40(DFS)
+     *
+     *
+     *
+     * k-Sum问题: 15, 18 O(n^(k-1))
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
